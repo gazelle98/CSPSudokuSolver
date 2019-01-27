828201a3e64f14a998f2cdc33115f4b985f2eaab
Liam Douglass <liamadouglass@gmail.com>
2019-01-26 21:46:55 -0500

    modified set_element function in order to take advantage of ac-3 algorithm

--- sudoku_problem.py
+++ sudoku_problem.py
@@ -1,3 +1,6 @@
+import search
+
+
 class Sudoku:
     def __init__(self, sudoku_state):
         """
@@ -51,20 +54,25 @@ class Sudoku:
         else:
             self.sudoku_state[row][col] = entry
 
-            for i in range(9):
-                if entry in self.possible_values[row][i]:
-                    self.possible_values[row][i].remove(entry)
-                if entry in self.possible_values[i][col]:
-                    self.possible_values[i][col].remove(entry)
-
-            initial_square_row = (row // 3) * 3
-            initial_square_col = (col // 3) * 3
-            for sr in range(initial_square_row, initial_square_row + 3):
-                for sc in range(initial_square_col, initial_square_col + 3):
-                    if entry in self.possible_values[sr][sc]:
-                        self.possible_values[sr][sc].remove(entry)
+            # for i in range(9):
+            #     if entry in self.possible_values[row][i]:
+            #         self.possible_values[row][i].remove(entry)
+            #     if entry in self.possible_values[i][col]:
+            #         self.possible_values[i][col].remove(entry)
+            #
+            # initial_square_row = (row // 3) * 3
+            # initial_square_col = (col // 3) * 3
+            # for sr in range(initial_square_row, initial_square_row + 3):
+            #     for sc in range(initial_square_col, initial_square_col + 3):
+            #         if entry in self.possible_values[sr][sc]:
+            #             self.possible_values[sr][sc].remove(entry)
 
             self.possible_values[row][col] = [entry]
+            neighbors = self.get_neighbors(row, col)
+            arcs = []
+            for n in neighbors:
+                arcs.append(((row, col), n))
+            search.arc_consistency_3(self, arcs)
 
     def get_entry(self, row, col):
         """
